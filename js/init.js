81e716f160c8f2822d5df173f42c6632c7d455b4
Stephen Hanzlik <stephenhanzlik@Stephens-MacBook-Pro.local>
2016-10-29 13:29:51 -0600

    imbeded iframes in parallax so that maps are immediately veiwable

--- js/init.js
+++ js/init.js
@@ -1,10 +1,34 @@
+"use strict";
 (function($){
   $(function(){
 
+    //materialize scripts
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     // $('#modal1').openModal();
     $('.modal-trigger').leanModal();
+
+    // avalanche red danger icon
+    $('.fixed-action-btn').openFAB();
+
+
+    //AJAX to powderline API
+    var search = $('#searchButton');
+
+    search.on('click', function () {
+      console.log('hello');
+      event.preventDefault();
+      var $xhr = $.getJSON('http://api.powderlin.es/stations');
+      $xhr.done(function(data) {
+      if($xhr.status !== 200) {
+        return;
+      }
+      console.log('hello');
+      console.log(data);
+
+    });
+  });
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
