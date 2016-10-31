fcc693e5e731910cd4ee9de4407484620c6942fc
Stephen Hanzlik <stephenhanzlik@Stephens-MacBook-Pro.local>
2016-10-30 20:20:03 -0600

    improved BC zone collection.  adding forms.  icons are updated and buttons are used on main landing page replacing materialize icons.  buttons lowerd on map portions.

--- js/init.js
+++ js/init.js
@@ -1,6 +1,50 @@
 "use strict";
 (function($){
   $(function(){
+    //JSON graph
+//     var ctx = $("#myChart");
+//   //   Chart.defaults.global = {
+//   //     scaleOverride: true,
+//   //     scaleSteps: 5,
+//   //     scaleStepWidth: 12,
+//   //    scaleStartValue: 30,
+//   //  };
+//     var myChart = new Chart(ctx, {
+//     type: 'line',
+//     data: {
+//         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+//         datasets: [
+//       {
+//         label: "My First dataset",
+//         fillColor: "rgba(220,220,220,0.2)",
+//         strokeColor: "rgba(220,220,220,1)",
+//         pointColor: "rgba(220,220,220,1)",
+//         pointStrokeColor: "#fff",
+//         pointHighlightFill: "#fff",
+//         pointHighlightStroke: "rgba(220,220,220,1)",
+//         data: [65, 59, 80, 81, 56, 55, 40]
+//       }],
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero:true
+//                 }
+//             }]
+//         }
+//     }
+// });
+// myChart.defaults.global = {
+//   scaleOverride: true,
+//   scaleSteps: 5,
+//   scaleStepWidth: 12,
+//   scaleStartValue: 30,
+// };
+   //Collapsable collection script
+    $(document).ready(function(){
+    $('.collapsible').collapsible();
+    });
 
     //materialize scripts
     $('.button-collapse').sideNav();
@@ -11,6 +55,11 @@
     // avalanche red danger icon
     $('.fixed-action-btn').openFAB();
 
+    // Date entry script
+    $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
 
     //AJAX to powderline API
     var search = $('#searchButton');
