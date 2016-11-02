1e21f7de28ded6eb3d8c5b03226f36f4909e38a5
Stephen Hanzlik <stephenhanzlik@Stephens-MacBook-Pro.local>
2016-11-02 14:30:43 -0600

    24hr snowfall graph is working.  fixed layout of landing page.  cards are displaye with 24hr snow total

--- js/init.js
+++ js/init.js
@@ -39,9 +39,7 @@
     var $temp4 = $('.temp4');
 
     $(document).ready(function() {
-        // var stationArr = ["564:CO:SNTL", "335:CO:SNTL", "1042:CO:SNTL", "322:CO:SNTL", "415:CO:SNTL"];
-        // for (var i = 0; i < stationArr.length; i++) {
-        //     var stationID = stationArr[i];
+        //Manipulate DOM on index page
         var $xhr = $.getJSON('https://g-powderlines.herokuapp.com/station/564:CO:SNTL');
         $xhr.done(function(data) {
             if ($xhr.status === 200 || $xhr.status === undefined) {
@@ -55,9 +53,9 @@
                 return;
             }
         });
-        var $xhr = $.getJSON('https://g-powderlines.herokuapp.com/station/335:CO:SNTL');
-        $xhr.done(function(data) {
-            if ($xhr.status === 200 || $xhr.status === undefined) {
+        var $xhr1 = $.getJSON('https://g-powderlines.herokuapp.com/station/335:CO:SNTL');
+        $xhr1.done(function(data) {
+            if ($xhr1.status === 200 || $xhr1.status === undefined) {
                 $snow2.empty();
                 $snow2.append(data.station_information.name);
                 $temp2.empty();
@@ -68,9 +66,9 @@
                 return;
             }
         });
-        var $xhr = $.getJSON('https://g-powderlines.herokuapp.com/station/1042:CO:SNTL');
-        $xhr.done(function(data) {
-            if ($xhr.status === 200 || $xhr.status === undefined) {
+        var $xhr2 = $.getJSON('https://g-powderlines.herokuapp.com/station/322:CO:SNTL');
+        $xhr2.done(function(data) {
+            if ($xhr2.status === 200 || $xhr2.status === undefined) {
                 $snow3.empty();
                 $snow3.append(data.station_information.name);
                 $temp3.empty();
@@ -81,9 +79,9 @@
                 return;
             }
         });
-        var $xhr = $.getJSON('https://g-powderlines.herokuapp.com/station/322:CO:SNTL');
-        $xhr.done(function(data) {
-            if ($xhr.status === 200 || $xhr.status === undefined) {
+        var $xhr3 = $.getJSON('https://g-powderlines.herokuapp.com/station/322:CO:SNTL');
+        $xhr3.done(function(data) {
+            if ($xhr3.status === 200 || $xhr3.status === undefined) {
                 $snow4.empty();
                 $snow4.append(data.station_information.name);
                 $temp4.empty();
@@ -95,118 +93,264 @@
             }
         });
 
+      //Average 5x stations from each zone an render to CAIC page
+          //front range stations
+        var $avrgFrontRange = $('avrgFrontRange');
+        var avrgFrontRangeArr = [];
 
-        //plot monthly snowpack depth to array for graph graphing
-        var $stationSearch = $(".statSearch");
-        var $select = $('#select');
-        var $station2Val = $(".statSearch2").val();
-        var $station3Val = $(".statSearch3").val();
-        var searchVal = 0;
-        $stationSearch.on("click", function () {
-          event.preventDefault();
-          searchVal = $select.val();
-
- //
- // });
-
-
-
-        // $stationSearch.on('click', function(){
-        // $(this).attr('action',$(this).attr('action')+$('#agent').val());
-        // $(this).submit();
-        // });
-
-
-        var monthlySnow = [];
-        var dates = [];
-        var $xhr = $.getJSON('https://g-powderlines.herokuapp.com/station/' + searchVal +':CO:SNTL?days=365');
-        $xhr.done(function(data) {
-            if ($xhr.status === 200 || $xhr.status === undefined) {
-              //push snow pack depth at monthly intervals
-                monthlySnow.push(data.data[29]["Snow Depth (in)"] + '"');
-                dates.push(data.data[29].Date);
-                monthlySnow.push(data.data[60]["Snow Depth (in)"] + '"');
-                dates.push(data.data[60].Date);
-                monthlySnow.push(data.data[91]["Snow Depth (in)"] + '"');
-                dates.push(data.data[91].Date);
-                monthlySnow.push(data.data[120]["Snow Depth (in)"] + '"');
-                dates.push(data.data[120].Date);
-                monthlySnow.push(data.data[151]["Snow Depth (in)"] + '"');
-                dates.push(data.data[151].Date);
-                monthlySnow.push(data.data[181]["Snow Depth (in)"] + '"');
-                dates.push(data.data[181].Date);
-                monthlySnow.push(data.data[212]["Snow Depth (in)"] + '"');
-                dates.push(data.data[212].Date);
-                monthlySnow.push(data.data[242]["Snow Depth (in)"] + '"');
-                dates.push(data.data[242].Date);
-                monthlySnow.push(data.data[273]["Snow Depth (in)"] + '"');
-                dates.push(data.data[273].Date);
-                monthlySnow.push(data.data[304]["Snow Depth (in)"] + '"');
-                dates.push(data.data[304].Date);
-                monthlySnow.push(data.data[334]["Snow Depth (in)"] + '"');
-                dates.push(data.data[334].Date);
-                monthlySnow.push(data.data[365]["Snow Depth (in)"] + '"');
-                dates.push(data.data[365].Date);
-
-              //push temperature at 5x monthly intervals
-
-
-
-              //convert monthly snow fall strings to number format
-                var newSnow = [];
-                var numArr = [];
-                for (var i = 0; i < monthlySnow.length; i++) {
-                    newSnow.push(monthlySnow[i].replace('"', '.'));
-                    if (!isNaN(parseInt(newSnow[i], 10))) {
-                        numArr.push(parseInt(newSnow[i], 10));
-                    } else {
-                        newSnow.push(0);
-                        numArr.push(0);
-                    }
-                }
-
-                var chartData = {
-                    chart: {
-                        type: 'line'
-                    },
-                    title: {
-                        text: 'Anual Snow Pack'
-                    },
-                    subtitle: {
-                        text: 'Powderline API'
-                    },
-                    xAxis: {
-                        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-                    },
-                    yAxis: {
-                        title: {
-                            text: 'Inches'
-                        }
-                    },
-                    plotOptions: {
-                        line: {
-                            dataLabels: {
-                                enabled: true
-                            },
-                            enableMouseTracking: true
-                        }
-                    },
-                    series: [{
-                        name: 'Snow Pack Depth',
-                        data: numArr,
-                    }, {
-                        name: 'London',
-                        data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-                    }]
-                };
-                Highcharts.chart('container', chartData);
-
-
+        var $frontAJAX = $.getJSON('https://g-powderlines.herokuapp.com/station/322:CO:SNTL');
+        $frontAJAX.done(function(data) {
+            if ($frontAJAX.status === 200 || $frontAJAX.status === undefined) {
+                avrgFrontRangeArr.push(data.data[5]["Change In Snow Depth (in)"]);
             } else {
                 return;
             }
         });
-    });
+        var $frontAJAX = $.getJSON('https://g-powderlines.herokuapp.com/station/1042:CO:SNTL');
+        $frontAJAX.done(function(data) {
+            if ($frontAJAX.status === 200 || $frontAJAX.status === undefined) {
+                avrgFrontRangeArr.push(data.data[5]["Change In Snow Depth (in)"]);
+            } else {
+                return;
+            }
+        });
+        var $frontAJAX = $.getJSON('https://g-powderlines.herokuapp.com/station/415:CO:SNTL');
+        $frontAJAX.done(function(data) {
+            if ($frontAJAX.status === 200 || $frontAJAX.status === undefined) {
+                avrgFrontRangeArr.push(data.data[5]["Change In Snow Depth (in)"]);
+            } else {
+                return;
+            }
+        });
+
+        //this converts string array to numbers but is not working :
+        console.log(avrgFrontRangeArr);
+        var fRangeNumArr = [];
+        for (var c = 0; c < avrgFrontRangeArr.length; c++) {
+            fRangeNumArr.push(parseInt(avrgFrontRangeArr[c], 10));
+            // if (!isNaN(parseInt(avrgFrontRangeArr[c], 10))) {
+            //     fRangeNumArr.push(parseInt(avrgFrontRangeArr[c], 10));
+            // } else {
+            //   fRangeNumArr.push(0);
+            // }
+        }
+        console.log(fRangeNumArr);
+
+
+        //plot 24 hour snow to bar graph
+
+        var allRequests = [];
+
+        allRequests.push($.getJSON('https://g-powderlines.herokuapp.com/station/564:CO:SNTL'));
+
+
+        allRequests.push($.getJSON('https://g-powderlines.herokuapp.com/station/335:CO:SNTL'));
+
+
+        allRequests.push($.getJSON('https://g-powderlines.herokuapp.com/station/415:CO:SNTL'));
+
+        allRequests.push($.getJSON('https://g-powderlines.herokuapp.com/station/1042:CO:SNTL'));
+
+
+        Promise.all(allRequests).then(function (results) {
+          var dailySnowObj = {};
+          for (var i = 0; i < results.length; i++) {
+            dailySnowObj[results[i].station_information.name] = results[i].data;
+          }
+
+          var dailySnowArr = [];
+          dailySnowArr[0] = dailySnowObj["LAKE ELDORA"][5]["Change In Snow Depth (in)"];
+          dailySnowArr[1] = dailySnowObj["COPPER MOUNTAIN"][5]["Change In Snow Depth (in)"];
+          dailySnowArr[2] = dailySnowObj["BERTHOUD SUMMIT"][5]["Change In Snow Depth (in)"];
+          dailySnowArr[3] = dailySnowObj["WILD BASIN"][5]["Change In Snow Depth (in)"];
+
+          var dailySnowNumArr = [];
+          for (var c = 0; c < dailySnowArr.length; c++) {
+              if (!isNaN(parseInt(dailySnowArr[c], 10))) {
+                  dailySnowNumArr.push(parseInt(dailySnowArr[c], 10));
+              }
+          }
+
+          console.log(dailySnowNumArr);
+
+          // var dailySnowArr = Object.keys(dailySnowObj).map(function (key) { return dailySnowObj[key]; });
+          // console.log(dailySnowArr);
+
+          var chartData2 = {
+            chart: {
+              type: 'bar'
+            },
+            title: {
+              text: 'New Snow Report 24 Hours'
+            },
+            subtitle: {
+              text: 'Powderline API'
+            },
+            xAxis: {
+              categories: ['Eldora', 'Copper', 'Berthoud', 'RMNP']
+            },
+            yAxis: {
+              title: {
+                text: 'Inches'
+              }
+            },
+            plotOptions: {
+              line: {
+                dataLabels: {
+                  enabled: true
+                },
+                enableMouseTracking: true
+              }
+            },
+            series: [{
+              name: 'Snow Pack Depth',
+              data: dailySnowNumArr
+            }]
+          };
+          Highcharts.chart('container2', chartData2);
+
+
+        });
+
+
+
+        //plot monthly snowpack depth to array for graph graphing
+        var $stationSearch = $(".statSearch");
+        var $select = $('#select');
+        var searchVal = 322;
+
+        $stationSearch.on("click", function() {
+            event.preventDefault();
+            searchVal = $select.val();
+
+
+            //collect snow pack observations from the end of the month
+            var monthlySnow = [];
+            var dates = [];
+
+
+
+            var $xhr = $.getJSON('https://g-powderlines.herokuapp.com/station/' + searchVal + ':CO:SNTL?days=365');
+            $xhr.done(function(data) {
+                if ($xhr.status === 200 || $xhr.status === undefined) {
+                    //push snow pack depth at monthly intervals
+                    monthlySnow.push(data.data[29]["Snow Depth (in)"] + '"');
+                    dates.push(data.data[29].Date);
+                    monthlySnow.push(data.data[60]["Snow Depth (in)"] + '"');
+                    dates.push(data.data[60].Date);
+                    monthlySnow.push(data.data[91]["Snow Depth (in)"] + '"');
+                    dates.push(data.data[91].Date);
+                    monthlySnow.push(data.data[120]["Snow Depth (in)"] + '"');
+                    dates.push(data.data[120].Date);
+                    monthlySnow.push(data.data[151]["Snow Depth (in)"] + '"');
+                    dates.push(data.data[151].Date);
+                    monthlySnow.push(data.data[181]["Snow Depth (in)"] + '"');
+                    dates.push(data.data[181].Date);
+                    monthlySnow.push(data.data[212]["Snow Depth (in)"] + '"');
+                    dates.push(data.data[212].Date);
+                    monthlySnow.push(data.data[242]["Snow Depth (in)"] + '"');
+                    dates.push(data.data[242].Date);
+                    monthlySnow.push(data.data[273]["Snow Depth (in)"] + '"');
+                    dates.push(data.data[273].Date);
+                    monthlySnow.push(data.data[304]["Snow Depth (in)"] + '"');
+                    dates.push(data.data[304].Date);
+                    monthlySnow.push(data.data[334]["Snow Depth (in)"] + '"');
+                    // dates.push(data.data[334].Date);
+                    // monthlySnow.push(data.data[365]["Snow Depth (in)"] + '"');
+
+                    //convert monthly snow fall strings to number format
+                    var newSnow = [];
+                    var numArr = [];
+                    for (var i = 0; i < monthlySnow.length; i++) {
+                        newSnow.push(monthlySnow[i].replace('"', '.'));
+                        if (!isNaN(parseInt(newSnow[i], 10))) {
+                            numArr.push(parseInt(newSnow[i], 10));
+                        }
+                    }
+
+                    //push temperature at 3x a month interval
+                    // x3Temp.push(data.data[3]["Air Temperature Observed (degF)"]);
+                    // x3Temp.push(data.data[16]["Air Temperature Observed (degF)"]);
+                    // x3Temp.push(data.data[26]["Air Temperature Observed (degF)"]);
+                    // // conver 3x a month observations to numbers for graphing
+                    // var newTemp = [];
+                    // var tempNumArr = [];
+                    // for (var i = 0; i < x3Temp.length; i++) {
+                    //     newTemp.push(x3Temp[i]);
+                    //     if (!isNaN(parseInt(newTemp[i], 10))) {
+                    //         tempNumArr.push(parseInt(newTemp[i], 10));
+                    //     }
+                    // }
+
+                    //or grab all the years data and put them in seperate arrays every 30 days
+                    // var yearsTemp = [];
+                    // for (var j = 0; j < 365; j++) {
+                    //     yearsTemp.push(data.data[j]["Air Temperature Observed (degF)"]);
+                    // }
+                    // var nov15 = yearsTemp.splice(0, 28);
+                    // var dec15 = yearsTemp.splice(29, 59);
+                    // var jan16 = yearsTemp.splice(60, 90);
+                    // var feb16 = yearsTemp.splice(91, 119);
+                    // var mar16 = yearsTemp.splice(120, 150);
+                    // var apr16 = yearsTemp.splice(151, 180);
+                    // var may16 = yearsTemp.splice(181, 211);
+                    // var jun16 = yearsTemp.splice(212, 241);
+                    // var jul16 = yearsTemp.splice(242, 272);
+                    // var aug16 = yearsTemp.splice(273, 303);
+                    // var sept16 = yearsTemp.splice(304, 333);
+                    // var 16 = yearsTemp.splice(334, 364);
+                    // // for (var y = 0; y < 360; 30++) {
+                    // //   array[y]
+                    // // }
+                    // console.log("******"+yearsTemp);
+
+                    //push temperature at 5x monthly intervals
+
+
+
+                    //annual snowfall chart
+                    var chartData = {
+                        chart: {
+                            type: 'line'
+                        },
+                        title: {
+                            text: 'Anual Snow Pack'
+                        },
+                        subtitle: {
+                            text: 'Powderline API'
+                        },
+                        xAxis: {
+                            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov']
+                        },
+                        yAxis: {
+                            title: {
+                                text: 'Inches'
+                            }
+                        },
+                        plotOptions: {
+                            line: {
+                                dataLabels: {
+                                    enabled: true
+                                },
+                                enableMouseTracking: true
+                            }
+                        },
+                        series: [{
+                            name: 'Snow Pack Depth',
+                            data: numArr,
+                        }, {
+                            name: 'Average Temperature',
+                            data: []
+                        }]
+                    };
+                    Highcharts.chart('container', chartData);
+
+
+                } else {
+                    return;
+                }
+            });
+        });
         //
         // $(function () {
         //       Highcharts.chart('container', {
